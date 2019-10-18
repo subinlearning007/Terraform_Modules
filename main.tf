d4c6f004317c91c04d3c29a8c8899b7edb4922f9
subinaws <37502606+subinaws@users.noreply.github.com>
2019-10-18 13:07:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 ### Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-vijith-test"
+    bucket = "terraform-learning"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
